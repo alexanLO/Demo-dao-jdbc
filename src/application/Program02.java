089b027249113c34010f98d9a029f1abc0cb9d01
Alexan Lima <alexan.works@gmail.com>
2022-02-08 15:56:51 -0300

    Implementando a function FindAll do DepartmentDao

--- src/application/Program02.java
+++ src/application/Program02.java
@@ -1,11 +1,12 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 import models.dao.DaoFactory;
 import models.dao.DepartmentDao;
 import models.entities.Department;
-import models.entities.Seller;
 
 public class Program02 {
     public static void main(String[] args) throws Exception {
@@ -16,6 +17,14 @@ public class Program02 {
         System.out.println("=== Teste 01: Department FindById ===");
         Department department = departmentDao.findById(3);      
         System.out.println(department);
+        
+        System.out.println();
+
+        System.out.println("=== Teste 02: Department FindAll ===");
+        List<Department> departments = departmentDao.findAll();     
+        for (Department department2 : departments) {
+            System.out.println(department2);
+        }
         
         System.out.println();
         
--- src/models/dao/implementacao/DepartmentDaoJDBC.java
+++ src/models/dao/implementacao/DepartmentDaoJDBC.java
@@ -4,7 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import db.DB;
 import db.DbException;
@@ -71,8 +74,32 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 
     @Override
     public List<Department> findAll() {
-        // TODO Auto-generated method stub
-        return null;
+        PreparedStatement st = null;
+        ResultSet rs = null;
+
+        try {
+            st = connection.prepareStatement(
+                    "SELECT * FROM department ORDER BY Name");
+
+        rs = st.executeQuery();
+
+        List<Department> departments = new ArrayList<>();
+
+        while(rs.next()){
+           Department department = new Department();
+           department.setId(rs.getInt("Id"));
+           department.setName(rs.getString("Name"));
+           departments.add(department);
+        }
+
+        return departments;
+
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+        }
     }
 
 }
